24cd0982674e6cc4685a38b585cbc8cf64b1b2f6
Ali Muhammad <alimuhammad5921@gmail.com>
2025-11-29 12:01:10 +0500

    DOM manipluation

---
+++ 005.js
@@ -0,0 +1,8 @@
+var element ; 
+document.querySelector('#header').innerHTML = <h1>SPY X FAMILY</h1>
+
+element = document.querySelector('#header').getAttribute('class'); ;
+
+console.log(element);
+
+element = document.querySelector('#header').style.border ;
